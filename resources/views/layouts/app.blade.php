e9536348d8d807e18b3ea676385d2bda03404d3e
thabzzzzz <tmosenohi99@gmail.com>
2023-11-27 07:36:05 +0200

    no message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,10 +93,11 @@
             @yield('content')
         </main>
     </div>
-    <div class="footer row">
-        <div class="col-4 footer-col-1">g</div>
-        <div class="col-4 footer-col-2">g</div>
-        <div class="col-4 footer-col-3">g</div>
-    </div>
+   
 </body>
+<div class="footer row">
+    <div class="col-4 footer-col-1">g</div>
+    <div class="col-4 footer-col-2">g</div>
+    <div class="col-4 footer-col-3">g</div>
+</div>
 </html>
